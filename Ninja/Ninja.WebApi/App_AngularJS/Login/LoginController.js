5b49abf546ae926891596c5ab8bdca803094801b
Josip Baketarić <josipba@in2.hr>
2017-10-03 14:25:59 +0200

    Addded login in angular

--- Ninja/Ninja.WebApi/App_AngularJS/Login/LoginController.js
+++ Ninja/Ninja.WebApi/App_AngularJS/Login/LoginController.js
@@ -1,1 +1,21 @@
-﻿
+﻿angular.module('appModule').controller('LoginController', ['$scope', '$http', function ($scope, $http) {
+
+
+
+    $scope.LoginNinja = function () {
+
+
+
+        $http.get('/api/login/LoginNinja?name=' + $scope.name + "&password=" + $scope.password).then(function (data) {
+            $scope.countries = data.data;
+
+            console.log(data.data);
+        });
+
+
+    };
+
+
+
+}
+]);
